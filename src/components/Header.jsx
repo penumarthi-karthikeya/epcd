4064b2919f661db62349fcb98f96fa1688136685
Penumarthi karthikeya <146359693+penumarthi-karthikeya@users.noreply.github.com>
2024-09-21 15:57:19 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,7 +71,7 @@ const Header = () => {
           <li><a href="# ">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#prediction">Prediction</a></li>
-          <li><a href="#contact">Our Team</a></li>
+          <li><a href="#our_team">Our Team</a></li>
         </ul>
         <div className="icons">
           <div className="icon1">
